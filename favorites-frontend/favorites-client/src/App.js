8bf232c7352d098dc1c987cef63e3301c87f2f86
ktalmage <ktalmage85@gmail.com>
2020-08-10 14:46:21 -0400

    added mapStateToProps allowing component to access this state, mapped over states array

--- favorites-frontend/favorites-client/src/App.js
+++ favorites-frontend/favorites-client/src/App.js
@@ -3,15 +3,27 @@ import { connect } from 'react-redux'
 import './App.css';
 
 class App extends Component {
+  componentDidMount(){
+    
+  }
   render(){
+    const states = this.props.states.map((usstate, i) => <li key={i}>{usstate.name}</li>)
     return (
       <div className="App">
         <h2>Favorite Cities</h2>
+        <ul>
+          {this.props.loading ? <h4>Loading...</h4> : states}
+        </ul>
       </div>
       );
     }
   }
   
 
-
-export default connect()(App);
+const mapStateToProps = state => {
+  return{
+    states: state.stateReducer.states,
+    loading: state.stateReducer.loading
+  }
+}
+export default connect(mapStateToProps)(App);
