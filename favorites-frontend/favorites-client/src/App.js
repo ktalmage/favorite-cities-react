590a90d4d7b6595e2ee8fd0398a28bdbb9c6c5e7
ktalmage <ktalmage85@gmail.com>
2020-08-10 14:04:14 -0400

    adjusted index js file for Provider

--- favorites-frontend/favorites-client/src/App.js
+++ favorites-frontend/favorites-client/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h2>Favorite Cities</h2>
     </div>
   );
 }
--- favorites-frontend/favorites-client/src/index.js
+++ favorites-frontend/favorites-client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
