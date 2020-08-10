714a6e6ebe196031fc1b8a4afa179538bba43909
ktalmage <ktalmage85@gmail.com>
2020-08-10 14:09:56 -0400

    added redux devtools

--- favorites-frontend/favorites-client/src/index.js
+++ favorites-frontend/favorites-client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import './index.css';
 import App from './App';
